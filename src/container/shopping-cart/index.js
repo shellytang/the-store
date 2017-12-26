615f1e694048df8d34153dccaa94d0e94223781a
Shelly Tang <shellytang@gmail.com>
2017-12-26 14:33:28 -0800

    added subtotal and fixed rendering for empty shopping cart

--- src/container/shopping-cart/index.js
+++ src/container/shopping-cart/index.js
@@ -9,13 +9,19 @@ class ShoppingCart extends Component {
   render () {
 
     const { cart, items } = this.props;
+    let subTotal = 0;
+
     let cartList = cart.itemIds.map(id => {
+      let itemTotal = cart.quantityById[id] * items[id].price;
+      subTotal+= itemTotal;
+
       return (
         <div key={id}>
           <p>{items[id].name}</p>
           <p>{items[id].description}</p>
           <p>${items[id].price}</p>
           <p>Quantity: {cart.quantityById[id]}</p>
+          <p>Total: ${itemTotal}</p>
           <button onClick={() => this.props.removeItem(id, this.props.updateQty(0, id))}>Remove</button>
           <Link to={`products/${id}`}>Edit item</Link>
         </div>
@@ -25,11 +31,14 @@ class ShoppingCart extends Component {
     return (
       <div>
         <h2>This is the shopping cart!</h2>
-        { cart.length === 0 ? <div>Cart is empty</div> :
+        { cart.itemIds.length === 0 ? <div>Cart is empty</div> :
           <div>
             <ul>
               {cartList}
             </ul>
+            <div>
+              <p>Subtotal: ${subTotal}</p>
+            </div>
           </div>
         }
         <Link to='/'>Back to products</Link>
